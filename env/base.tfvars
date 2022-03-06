c77252d53ed329c5acf1438c212c6a24c5f0ccef
Branden Ward <brandencward@gmail.com>
2022-03-06 13:43:05 -0600

    Minor Fixes

--- env/base.tfvars
+++ env/base.tfvars
@@ -1,8 +1,8 @@
 // Variables for specfic invironment that gets referenced in the command line.
 // Works with variables.tf
-rg_name = "base resources"
+rg_name = "base-resources"
 rg_location = "Central US"
-asa_name = "learningstorageaccountbward125"
+asa_name = "storageaccountbward"
 asa_account_tier = "Standard"
 asa_account_replication_type = "GRS"
 asa_tags = {
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 // Setting the varibales like this will allow you to input via command line or file. Ex: rg_name
-rg_name = {}
-rg_location = {}
-asa_name = {}
-asa_account_tier = {}
-asa_account_replication_type = {}
-asa_tags = {}
+variable rg_name {}
+variable rg_location {}
+variable asa_name {}
+variable asa_account_tier {}
+variable asa_account_replication_type {}
+variable asa_tags {}
